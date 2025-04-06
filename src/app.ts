08a6d5395aa208c2536361865d05a47a8b7f6231
Alireza-QK <alireza.QK.75@gmail.com>
2025-04-06 12:41:57 +0330

    add routes to app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
 import { connectDB } from "./config/db";
+import { Routes } from "./routes";
 
 export async function buildApp() {
   const app = fastify({ logger: true });
@@ -13,6 +14,8 @@ export async function buildApp() {
   });
 
   // Define routes here
+  app.register(Routes)
+  
   app.get("/ok", (req, res) => {
     res.send({ message: "Success" });
   });
